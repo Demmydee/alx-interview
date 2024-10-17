dd5fd2c8391bf360fe5fa666feb88c04cefe997b
Olaoye Ademola <demmydd44@gmail.com>
2024-10-16 20:04:52 -0700

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Minimum operations task
+"""
+def minOperations(n):
+    copy = 1
+    x = 1
+    op_count = 0
+    while x < n:
+        x += copy
+        op_count += 1
+        if n%copy == 0:
+            copy = x
+            op_count += 1
+
+    return(op_count)
